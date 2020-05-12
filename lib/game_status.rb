3aa219b7d212eb32ea111be3d278a743a5e749f0
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-12 21:19:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,5 +18,17 @@ WIN_COMBINATIONS = [
 #won method should accept board as an argument
 
 def won?(board)
-  
+  WIN_COMBINATIONS.each do |win_combination|
+    
+    win = []
+    if win_combination == ["X","X","X"] || win_combination == ["O","O","O"]
+      win_combination.each do |position|
+        win.push ("#{position}")
+      end 
+      return "#{win} Wins!"
+      
+      
+      
+    
+    
 end
