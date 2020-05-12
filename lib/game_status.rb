379f2e8d97d7b0efab0d5a3877c2924fa1f747b3
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-12 21:23:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
+    board.empy? == [" "," "," "," "," "," "," "," "," "]
     win = []
     if win_combination == ["X","X","X"] || win_combination == ["O","O","O"]
       win_combination.each do |position|
